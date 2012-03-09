265cdf9ac56e9a19653e0e21f33e07e5e322ca8d
Lucas Fais <lucas.fais@abril.com.br>
2012-03-09 10:11:15 -0300

    New version with 'fais' sufix.

--- lib/tokamak/version.rb
+++ lib/tokamak/version.rb
@@ -2,9 +2,10 @@ module Tokamak
   module VERSION #:nodoc:
     MAJOR = 1
     MINOR = 2
-    TINY  = 1
+    TINY  = 2
+    SUFIX = 'fais'
 
-    STRING = [MAJOR, MINOR, TINY].join('.')
+    STRING = [MAJOR, MINOR, TINY].join('.') + "-#{SUFIX}"
 
     def self.to_s
       STRING
